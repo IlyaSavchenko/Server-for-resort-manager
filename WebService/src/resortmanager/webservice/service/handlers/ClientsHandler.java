ac9e4ff4b525fdad257c5b9181935873287edf31
IlyaSavchenko <sav.il.yu@gmail.com>
2013-11-09 21:26:32 +0400

    coolCommit

--- WebService/src/resortmanager/webservice/service/handlers/ClientsHandler.java
+++ WebService/src/resortmanager/webservice/service/handlers/ClientsHandler.java
@@ -3,13 +3,9 @@ package resortmanager.webservice.service.handlers;
 import resortmanager.data.models.Client;
 import resortmanager.webservice.dal.ConnectionSingleton;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import java.sql.Connection;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
 
 /**
@@ -70,6 +66,62 @@ public class ClientsHandler {
         }
     }
 
+    @GET
+    @Produces("text/plain")
+    @Path("by_surname/{surname}")
+    public String ClientBySurname(@PathParam("surname") String surname){
+        try {
+            Statement statment = connection.createStatement();
+            ResultSet resultSet = statment.executeQuery("SELECT * FROM Clients WHERE cl_surname = '" + surname + "'");
+            String json = "[";
+            boolean firstRecord = true;
+            while (resultSet.next()){
+                if (firstRecord) {
+                    firstRecord = false;
+                }
+                else {
+                    json = json.concat(",");
+                }
+                Client client = new Client();
+                client.setId(resultSet.getInt("id_client"));
+                client.setName(resultSet.getString("cl_name"));
+                client.setMiddlename(resultSet.getString(("cl_middlename")));
+                client.setSurname(resultSet.getString("cl_surname"));
+                client.setBirthday(resultSet.getDate("cl_birthday"));
+                client.getPassport(resultSet.getInt("cl_passport"));
+                json = json.concat(client.ToJSON());
+            }
+            if (!firstRecord) {
+                return json.concat("]");
+            }
+            else {
+                throw new Exception();
+            }
+        } catch (Exception e) {
+            return "ERROR";  //To change body of catch statement use File | Settings | File Templates.
+        }
+    }
 
+    @GET
+    @Produces("text/plain")
+    @Path("register")
+    public String ClientRegister(@QueryParam("name") String name, @QueryParam("surname") String surname, @QueryParam("middlename") String middlename, @QueryParam("passport") String passport){
+        try {
+            Statement statment = connection.createStatement();
+            statment.executeUpdate("INSERT INTO Clients('cl_name, cl_surname, cl_middlename, cl_passport') " +
+                    "VALUES ('" + name + "', '" + surname + "','" + middlename +"', " + passport + ")");
+            //throw new Exception();
+            return "register success";
+        } catch (Exception e) {
 
+            return "ERROR";  //To change body of catch statement use File | Settings | File Templates.
+        }
+    }
 }
+
+
+
+
+
+
+
